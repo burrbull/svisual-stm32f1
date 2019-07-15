0bb490ff4e11e4c4f6d2b5d46e4f65bf4bf38386
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2019-07-15 08:26:08 +0300

    bump svisual, heapless

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ where
         let packet_size = P::to_usize();
         let vl_size : usize = NAME_SZ+4+packet_size*4;
         static mut NDATA : MaybeUninit<[u8; NAME_SZ+4]> = MaybeUninit::uninit();
-        let mut val_data = MaybeUninit::<GA<P>>::uninit();// should be static
+        let mut val_data = unsafe { MaybeUninit::<GA<P>>::uninit().assume_init() };// should be static
         unsafe {
             // Full package size
             let x_arr = &mut *NDATA.as_mut_ptr();
@@ -84,10 +84,11 @@ where
 
                 self.write_and_wait(&(*NDATA.as_ptr()));
 
-                LE::write_i32_into(&v.vals, (&mut *val_data.as_mut_ptr()).as_mut_slice());
 
-                self.write_and_wait(&(*val_data.as_ptr()));
             }
+
+            LE::write_i32_into(&v.vals, &mut val_data.as_mut_slice());
+            self.write_and_wait(&val_data);
         }
         self.write_and_wait(b"=end=");
     }
